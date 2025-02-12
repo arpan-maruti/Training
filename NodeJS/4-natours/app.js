a32f61ca39f2d21fcfc6b223e5122f76e7359d66
arpan-maruti <arpan.rupareliya@marutitech.com>
2025-02-12 13:19:37 +0530

    chore: minor changes

--- NodeJS/4-natours/app.js
+++ NodeJS/4-natours/app.js
@@ -17,7 +17,8 @@ app.use(express.json());
 const tours = JSON.parse(
     fs.readFileSync(`./starter/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res)=>{
+
+const getTours =(req, res)=>{
     res.status(200).json({
         status: "success",
         results: tours.length,
@@ -25,13 +26,93 @@ app.get('/api/v1/tours', (req, res)=>{
             tours
         }
     });
-});
+}
 
-app.post('/api/v1/tours', (req,res)=>{
-    console.log(req.body);
-    res.send('Done');
+const getTourById =(req, res)=>{
+    const id = req.params.id*1;
+    const tour =tours.find(el=>el.id === id);
+    if(!tour)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tour
+        }
+    });
+}
 
-});
+const postUser =(req,res)=>{
+    const newId = tours[tours.length -1].id +1;
+    const newTour = Object.assign({id: newId}, req.body);
+ 
+    tours.push(newTour);
+ 
+    fs.writeFile(
+     `./starter/dev-data/data/tours-simple.json`,
+     JSON.stringify(tours),
+     err=>{
+         res.status(201).json({
+             ststus:'success',
+             data:{
+                 tour:newTour,
+             }
+         })
+     }
+    )
+ }
+
+ const patchUser =(req,res)=>{
+
+    const id = req.params.id*1;
+    const tour =tours.find(el=>el.id === id);
+    if(!tour)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: '<updated>'
+        }
+    });
+
+}
+
+const deleteUser = (req,res)=>{
+
+    const id = req.params.id*1;
+    const tour =tours.find(el=>el.id === id);
+    if(!tour)
+    {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(204).json({
+        status: "success",
+        data: {
+            tour: '<updated>'
+        }
+    });
+
+}
+
+
+
+app.route('/api/v1/tours').get(getTours).post(postUser);
+app.route('/api/v1/tours/:id').get(getTourById).patch(patchUser).delete(deleteUser);
+
+
 app.listen(port, ()=>{
     console.log(`App Running on port ${port}`);
 });
