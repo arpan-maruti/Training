a68623c726b7a07d1c9aaafa9473e5193da20ec1
arpan-maruti <arpan.rupareliya@marutitech.com>
2025-03-26 09:54:34 +0530

    feat: useRef example added

--- ReactJS/react-training/src/hooks/UseRef.jsx
+++ ReactJS/react-training/src/hooks/UseRef.jsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from "react";
 export default function UseRef() {
   const [count, setCount] = useState(0);
   const timerRef = useRef(null);
+  const inputRef = useRef(null); 
 
   function handleStart() {
     if (!timerRef.current) {
@@ -11,27 +12,37 @@ export default function UseRef() {
       }, 1000);
     }
   }
+
   function handleStop() {
     clearInterval(timerRef.current);
-    timerRef.current = null; 
+    timerRef.current = null;
   }
 
   function handleReset() {
     clearInterval(timerRef.current);
     timerRef.current = null;
-    setCount(0); 
+    setCount(0);
   }
+
+  function handleFocusInput() {
+    inputRef.current.focus(); 
+  }
+
   return (
     <>
       <h1>useRef</h1>
       <hr />
       <h1> Stopwatch : {count} </h1>
-
       <div style={{ display: "flex", gap: "0.5rem" }}>
         <button onClick={handleStart}>start</button>
         <button onClick={handleStop}>stop</button>
         <button onClick={handleReset}>reset</button>
       </div>
+
+      <hr />
+      <h2>Using useRef to access DOM</h2>
+      <input ref={inputRef} type="text" placeholder="Type here..." />
+      <button onClick={handleFocusInput}>Focus Input</button>
     </>
   );
 }
